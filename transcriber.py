9ce21c88fb0f4a996102d215031f6887cc7e7ea5
Zhakhangir Anuarbek <zhaxa65@gmail.com>
2024-10-20 13:28:11 +0900

    Update .gitignore and transcriber.py
    - Update .gitignore to include audio/ and textfiles/ directories.
    - Update transcriber.py to use GPU with float16 compute type and transcribe a different audio file.
    - Update transcriber.py to write the transcript file with UTF-8 encoding.

--- transcriber.py
+++ transcriber.py
@@ -3,18 +3,18 @@ from faster_whisper import WhisperModel
 model_size = "large-v3"
 
 # Run on GPU with FP16
-model = WhisperModel(model_size, device="auto", compute_type="int8")
+model = WhisperModel(model_size, device="cuda", compute_type="float16")
 
 # or run on GPU with INT8
 # model = WhisperModel(model_size, device="cuda", compute_type="int8_float16")
 # or run on CPU with INT8
 # model = WhisperModel(model_size, device="cpu", compute_type="int8")
 
-segments, info = model.transcribe("audio/test.mp3", beam_size=5)
+segments, info = model.transcribe("audio/space_lecture_2nd_part1.mp3", beam_size=5)
 
 print("Detected language '%s' with probability %f" % (info.language, info.language_probability))
 
-with open("textfiles/transcript.txt", "w") as file:
+with open("textfiles/transcript.txt", "w", encoding="utf-8") as file:
     for segment in segments:
         file.write("[%.2fs -> %.2fs] %s\n" % (segment.start, segment.end, segment.text))
-        print("[%.2fs -> %.2fs] %s" % (segment.start, segment.end, segment.text)) 
+        print("[%.2fs -> %.2fs] %s" % (segment.start, segment.end, segment.text))
